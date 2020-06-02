c8323d5f2eb05b9a96fd8fd92ad0e3caa93d5483
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-02 19:22:58 -0300

    feat: add seeds directory in knexfile

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -11,5 +11,8 @@ module.exports = {
   migrations: {
     directory: resolve(__dirname, 'src', 'database', 'migrations'),
   },
+  seeds: {
+    directory: resolve(__dirname, 'src', 'database', 'seeds'),
+  },
   useNullAsDefault: true,
 };
