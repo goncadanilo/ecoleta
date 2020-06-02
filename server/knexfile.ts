4ceae8be49e1b3ce5f45ba1e95e50df93fb2b31d
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-02 18:44:41 -0300

    chore: create knexfile

---
+++ server/knexfile.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+
+const { NODE_ENV } = process.env;
+const database = NODE_ENV === 'test' ? 'test.sqlite' : 'database.sqlite';
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'src', 'database', database),
+  },
+  migrations: {
+    directory: resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+};
