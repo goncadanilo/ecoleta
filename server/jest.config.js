b6b3179b9d3fee04ef7cb7fc2606e55d50f07911
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-03 12:04:35 -0300

    chore: update jest settings

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!src/server.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text-summary', 'lcov'],
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/__tests__/**/*.spec.ts'],
+  testMatch: ['<rootDir>/__tests__/*.spec.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
