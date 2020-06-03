a664fb3089eb734c70616fceeea3277f198c0098
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-03 12:22:50 -0300

    refactor: update database connection

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,13 +1,11 @@
 import { resolve } from 'path';
 
-const { NODE_ENV } = process.env;
-const database = NODE_ENV === 'test' ? 'test.sqlite' : 'database.sqlite';
 const path = resolve(__dirname, 'src', 'database');
 
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: resolve(path, database),
+    filename: resolve(path, 'database.sqlite'),
   },
   migrations: {
     directory: resolve(path, 'migrations'),
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,13 +1,10 @@
 import knex from 'knex';
 import { resolve } from 'path';
 
-const { NODE_ENV } = process.env;
-const database = NODE_ENV === 'test' ? 'test.sqlite' : 'database.sqlite';
-
 const connection = knex({
   client: 'sqlite3',
   connection: {
-    filename: resolve(__dirname, database),
+    filename: resolve(__dirname, 'database.sqlite'),
   },
   useNullAsDefault: true,
 });
