620f9a1980bd68937cb36ff8707dcdc15ec5d86c
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-04 16:21:01 -0300

    feat: create AppError class

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { resolve } from 'path';
 
 import routes from './routes';
+import AppError from './errors/AppError';
 
 class App {
   public app: express.Application;
@@ -12,6 +13,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.handleErrors();
   }
 
   private middlewares(): void {
@@ -22,6 +24,16 @@ class App {
   private routes(): void {
     this.app.use('/v1', routes);
     this.app.use('/files', express.static(resolve(__dirname, '..', 'uploads')));
+  }
+
+  private handleErrors(): void {
+    this.app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+      if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+      }
+
+      return res.status(500).json({ message: 'Internal server error' });
+    });
   }
 }
 
