72e65507013091598bc0d95f840356c3f5a4d362
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-04 18:19:29 -0300

    chore: add express-async-errors

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { resolve } from 'path';
+import 'express-async-errors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
