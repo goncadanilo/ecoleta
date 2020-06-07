14d6c88896fc16fcc9b517481d9aa1c4bfb06c86
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-06 22:15:32 -0300

    feat: create Home page

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, ImageBackground, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require('../../assets/home-background.png')}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+}
+
+export default Home;
